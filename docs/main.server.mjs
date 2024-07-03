3978c016fc72017ed2c4211e501969a1518ead53
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2024-07-03 12:28:28 +0200

    main deploy

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-W55WDTMI.mjs";import"./chunk-NGGHYHNS.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'add-game-to-grouvee';
+  gameName: string = '';
+  
+  copyToClipboard(): void {
+    navigator.clipboard.writeText(`Fill up the following fields of the game: ${this.gameName}
+    
+    Name:
+    
+    Sort name: 
+    
+    Description: 
+    
+    Original Release Date: 
+    
+    Platforms: 
+    
+    Franchise (if they have more games of the same saga, if not, do not show this entry): 
+    
+    Developers: 
+    
+    Publishers: 
+    
+    Genres: `);
+  }
 }
